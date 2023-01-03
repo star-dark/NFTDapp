7307d9f458b36ee600617ead92f183101781f16b
star-dark <den1116@naver.com>
2023-01-03 12:07:36 +0900

    계정연결 버튼

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
-import detectEthereumProvider from '@metamask/detect-provider'
-const provider = await detectEthereumProvider();
-if (provider) {
-  // From now on, this should always be true:
-  // provider === window.ethereum   
-  startApp(provider); // initialize your app
-} else {
-  console.log('Please install MetaMask!');
+const initialize = () => {
+  //Basic Actions Section
+  const connectBtn = document.getElementById('connectButton');
+
+  //Created check function to see if the MetaMask extension is installed
+  const isMetaMaskInstalled = () => {
+    //Have to check the ethereum binding on the window object to see if it's installed
+    const { ethereum } = window;
+    return Boolean(ethereum && ethereum.isMetaMask);
+  };
+
+  const onClickConnect = async () => {
+  try {
+    // Will open the MetaMask UI
+    // You should disable this button while the request is pending!
+    await ethereum.request({ method: 'eth_requestAccounts' });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+  const MetaMaskClientCheck = () => {
+    //Now we check to see if MetaMask is installed
+    if (!isMetaMaskInstalled()) {
+      //If it isn't installed we ask the user to click to install it
+      connectBtn.innerText = 'Click here to install MetaMask!';
+      connectBtn.disabled = true;
+    } else {
+      //If it is installed we change our button text
+      connectBtn.innerText = 'Connect';
+      onboardButton.onclick = onClickConnect;
+      onboardButton.disabled = false;
+    }
+  };
+  MetaMaskClientCheck();
 }
-const ethereumButton = document.getElementById('connectButton');
-ethereumButton.addEventListener('click', () => {
-  //Will Start the metamask extension
-  ethereum.request({ method: 'eth_requestAccounts' });
-});
+window.addEventListener('DOMContentLoaded', initialize);
