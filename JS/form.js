db09811d462f43d29903a1e95f4f654cec78436a
star-dark <den1116@naver.com>
2023-01-06 13:41:45 +0900

    preventDefault

--- JS/form.js
+++ JS/form.js
@@ -1,12 +1,4 @@
 const makeItemForm = document.querySelector(".form");
 makeItemForm.addEventListener('submit', event =>{
     event.preventDefault();
-    const formdata = new FormData(makeItemForm);
-    const data = new URLSearchParams(formdata);
-    fetch('https://reqres.in/api/users',{
-        method : 'POST',
-        body : data
-    }).then(res => res.json())
-    .then(JSON.stringify(res))
-    .catch(error => console.error());
 });
