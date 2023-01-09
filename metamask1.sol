004c57665e83c9e71c21b13ee5924fbf8d106d39
nobiho <121214072+nobiho@users.noreply.github.com>
2023-01-09 10:34:59 +0900

    delete

--- metamask1.sol
+++
@@ -1,9 +0,0 @@
-if (typeof window.ethereum !== 'undefined') {
-    console.log('MetaMask is installed!');
-  }
-  ethereum.request({ method: 'eth_requestAccounts' });
-  <button class="enableEthereumButton">Enable Ethereum</button>
-  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-const account = accounts[0];
-// We currently only ever provide a single account,
-// but the array gives us some room to grow.
